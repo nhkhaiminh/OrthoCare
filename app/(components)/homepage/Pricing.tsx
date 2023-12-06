8ab45448c460d0c49e1bf6a1738064cdef0ac32b
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-12-06 14:38:43 -0500

    Add Pricing component

---
+++ app/(components)/homepage/Pricing.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import React from 'react'
+
+export const Pricing = () => {
+    return (
+        <div className='bg-gray-200 px-[2.5rem] lg:normalx py-20 grid justify-center'>
+            <div className="grid grid-cols-1 lg:grid-cols-2 border-cyan-800 border-2 rounded-tl-3xl rounded-br-3xl overflow-hidden">
+                <div className="flex flex-col gap-5 text-cyan-800 p-[2.5rem] md:p-[5rem] text-xl">
+                    <div className="relative">
+                        <Image
+                            className='w-[3.5rem] -translate-x-3'
+                            src={'/svgs/price-tag.svg'}
+                            width={100}
+                            height={100}
+                            alt='price tag'
+                        />
+                    </div>
+                    <span>TRANSPARENT PRICING</span>
+                    <h3 className='font-merry font-semibold text-4xl'>No Surprises</h3>
+                    <p>{`Our team believes in accessible dental care. We offer straightforward pricing, clear estimates, and an unprecedented dental warranty. Keep up with your routine 6-month checkups and if something needs fixing, we'll handle it at no cost to you.`}</p>
+                </div>
+                <div className="relative text-white bg-cyan-800 p-[2.5rem] md:p-[5rem] text-xl flex flex-col gap-5">
+                    <div className="block md:absolute md:bottom-0">
+                        <Image
+                            className='w-[3.5rem] md:w-[22rem] md:translate-y-[17rem] lg:translate-y-[10rem] -translate-x-3 md:translate-x-20'
+                            src={'/svgs/pricing-insurance.svg'}
+                            width={100}
+                            height={100}
+                            alt=''
+                        />
+                    </div>
+                    <h3 className='font-merry font-semibold text-2xl'>Insurance</h3>
+                    <p>Our office is in-network with Delta Premier and bills all other providers. Our team will explain how your specific dental benefits apply to the cost of treatment.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
