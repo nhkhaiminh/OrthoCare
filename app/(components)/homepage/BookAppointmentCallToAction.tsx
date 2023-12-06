0b7db1d708c81cfbd0c769f09340140a8a4cd7c0
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-12-06 14:39:25 -0500

    Add BookAppointmentCTA component

---
+++ app/(components)/homepage/BookAppointmentCallToAction.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import FadeBtn from '../FadeBtn'
+
+export default function BookAppointmentCallToAction() {
+    return (
+        <div className='relative grid justify-center bg-abstract-pattern text-white px-mobilex py-24'>
+            <div className="flex flex-col justify-center text-center gap-10">
+                <h3 className='font-merry font-semibold text-4xl md:text-5xl'>Book your visist online</h3>
+                <p className='text-xl'>View available appointments and enjoy dentistry done right. +FREE teeth whitening for life.</p>
+                <div className="">
+                    <FadeBtn>
+                        <Link href={'/'}>BOOK ONLINE</Link>
+                    </FadeBtn>
+                </div>
+            </div>
+        </div>
+    )
+}
