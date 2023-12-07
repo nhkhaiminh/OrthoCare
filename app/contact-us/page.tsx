d3b83eaa66bbeef298b1f46e82bb37a1fc291c80
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-12-07 13:19:13 -0500

    Add contact-us page component

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from '../(components)/Container'
+
+export default function ContactUsPage() {
+    return (
+        <div className=''>
+            <Container>
+                <p>Contact Us</p>
+            </Container>
+        </div>
+    )
+}
