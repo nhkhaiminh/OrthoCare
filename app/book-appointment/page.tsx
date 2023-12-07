4225dbff2365de74326b10d3c228a3c10305d0dd
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-12-07 13:19:34 -0500

    Create book-appointment page component

---
+++ app/book-appointment/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React, { useState } from 'react'
+import BookAppointmentForm from '../(components)/book-appointment/BookAppointmentForm'
+
+export default function BookAppointmentPage() {
+
+
+    return (
+        <div className='flex flex-col gap-8 pt-[7rem] pb-[23rem] px-mobilex lg:px-[20rem]'>
+            <h2 className='font-semibold text-3xl'>Request a Consultation</h2>
+            <BookAppointmentForm />
+        </div>
+    )
+}
