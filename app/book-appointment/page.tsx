62423e3647730fed6deac1700376b10aff77afac
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-12-08 15:49:33 -0500

    Fix responsive class issues

--- app/book-appointment/page.tsx
+++ app/book-appointment/page.tsx
@@ -7,7 +7,7 @@ export default function BookAppointmentPage() {
 
 
     return (
-        <div className='flex flex-col gap-8 pt-[7rem] pb-[23rem] px-mobilex lg:px-[20rem]'>
+        <div className='flex flex-col gap-8 pt-[7rem] pb-[23rem] px-mobilex lg:px-[10rem] xl:px-[20rem]'>
             <h2 className='font-semibold text-3xl'>Request a Consultation</h2>
             <BookAppointmentForm />
         </div>
