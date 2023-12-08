f372e7676b52197300c1044bfb22832766a2005f
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-12-08 15:50:47 -0500

    Fix small linting issue

--- app/book-appointment/success/page.tsx
+++ app/book-appointment/success/page.tsx
@@ -6,8 +6,8 @@ export default function page() {
         <div className='grid m-auto gap-5 pt-[7rem] pb-[23rem] px-mobilex lg:px-[10rem] xl:px-[20rem] max-w-7xl'>
             <div className="flex flex-col gap-3 rounded-lg px-2 md:px-10 py-20 md:py-12 border border-gray-300 drop-shadow-sm text-center">
                 <h2 className='font-semibold text-3xl'>We are sorry!</h2>
-                <p className='text-lg'>Unfortunately, OrthoCare is not a real dental clinic, and thus,
-                    your book appointment couldn't be processed.
+                <p className='text-lg'>{`Unfortunately, OrthoCare is not a real dental clinic, and thus,
+                    your book appointment couldn't be processed.`}
                 </p>
             </div>
             <Link href={'/'}>
