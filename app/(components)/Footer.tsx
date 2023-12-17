1bb50f5d6814afdfde0a577f7f58e8ce7dc26d74
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-12-17 13:22:50 -0500

    Update Footer component

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -3,22 +3,41 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 import React from 'react'
 
+interface SocialsBtnProps {
+    className?: string;
+    link?: string;
+    children: React.ReactNode
+}
+
+function SocialsBtn(props: SocialsBtnProps) {
+    return (
+        <Link href={props.link ? props.link : "/"}
+            className={props.className ? props.className :
+                "flex w-10 h-10 items-center justify-center rounded-full bg-white border-white border hover:bg-inherit duration-300"
+            }
+        >
+            {props.children}
+        </Link>
+    )
+}
+
 export default function Footer() {
     return (
-        <footer className='grid gap-10 space-y-8 px-[2rem] lg:px-[10rem] pb-16 pt-16 md:pb-8 md:pt-[4rem] bg-frblue text-white h-full align-bottom'>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 space-y-12 md:space-y-0">
+        <footer className='relative z-10 grid border-t border-sky-900 gap-10 space-y-8 px-[2rem] lg:px-[10rem] pb-16 pt-16 md:pb-8 md:pt-[4rem] bg-[#163472] text-white h-full align-bottom drop-shadow-md'>
+            <div className="absolute z-0 inset-0 polka-dots-footer"></div>
+            <div className="z-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 space-y-12 md:space-y-0">
                 <div className="">
                     <h3 className='font-medium text-2xl pb-3'>Socials</h3>
                     <div className="flex space-x-2">
-                        <Link className="flex w-10 h-10 items-center justify-center rounded-full bg-white border-white border hover:bg-sky-800 duration-300" href={'/'}>
-                            <FontAwesomeIcon className='w-6 h-6 text-frblue hover:text-white duration-300' icon={faFacebookF} />
-                        </Link>
-                        <Link className="flex w-10 h-10 items-center justify-center rounded-full bg-white border-white border hover:bg-sky-800 duration-300" href={'/'}>
-                            <FontAwesomeIcon className='w-6 h-6 text-frblue hover:text-white duration-300' icon={faInstagram} />
-                        </Link>
-                        <Link className="flex w-10 h-10 items-center justify-center rounded-full bg-white border-white border hover:bg-sky-800 duration-300" href={'/'}>
-                            <FontAwesomeIcon className='w-6 h-6 text-frblue hover:text-white duration-300' icon={faXTwitter} />
-                        </Link>
+                        <SocialsBtn>
+                            <FontAwesomeIcon className='w-6 h-6 text-blue-900 hover:text-white duration-300' icon={faFacebookF} />
+                        </SocialsBtn>
+                        <SocialsBtn>
+                            <FontAwesomeIcon className='w-6 h-6 text-blue-900 hover:text-white duration-300' icon={faInstagram} />
+                        </SocialsBtn>
+                        <SocialsBtn>
+                            <FontAwesomeIcon className='w-6 h-6 text-blue-900 hover:text-white duration-300' icon={faXTwitter} />
+                        </SocialsBtn>
                     </div>
                 </div>
                 <div className="justify-start">
@@ -69,7 +88,7 @@ export default function Footer() {
                     </div>
                 </div>
             </div>
-            <div className="">
+            <div className="z-20">
                 <p>© OrthoCare 2023. All Rights Reserved.</p>
                 <p>Website made by <a className='underline text-amber-200 hover:text-amber-300 duration-300' href='https://asleyrobleto.com' target={'_blank'}>Asley R</a>. 2023.</p>
             </div>
