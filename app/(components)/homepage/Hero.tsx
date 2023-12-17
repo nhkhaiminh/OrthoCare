d1a982a9cd714cc7f2f9721ae727a7c40b0dd915
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-12-17 13:25:03 -0500

    Add bg image and remove parallel image in hero section

--- app/(components)/homepage/Hero.tsx
+++ app/(components)/homepage/Hero.tsx
@@ -1,18 +1,18 @@
 "use client"
 
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { DateTimePicker } from '@mui/x-date-pickers';
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import FadeBtn from '../FadeBtn'
-import ComparisonSlider from '../ComparisonSlider';
+
+const bgImage = {
+    backgroundImage: "url('/media/dentist-operation.jpg')"
+}
 
 export default function Hero() {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-2 bg-cyan-700 pt-16'>
-            <div className="grid items-center px-mobilex md:px-normalx text-white pt-[5rem] pb-[3rem] lg:pb-[8rem]">
+        <div style={bgImage} className='relative z-10 bg-top bg-cover bg-fixed bg-no-repeat grid grid-cols-1 lg:grid-cols-2 pt-32 lg:pt-44'>
+            <div className="absolute bg-black/40 inset-0 -z-10"></div>
+            <div className="z-10 grid items-center px-mobilex md:px-normalx text-white pt-[5rem] pb-[3rem] lg:pb-[8rem]">
                 <div className="flex flex-col gap-8 md:gap-5">
                     <h1 className='font-merry font-semibold text-4xl md:text-5xl'>
                         A Dental Practice Designed with You in Mind. Finally.
@@ -28,14 +28,14 @@ export default function Hero() {
                 </div>
             </div>
             <div className="flex justify-end overflow-hidden relative px-mobilex md:px-normalx lg:px-0">
-                <Image
+                {/* <Image
                     className='w-full rounded-t-lg lg:rounded-none duration-200'
                     src={'/media/dentist-with-patient-2.jpg'}
                     width={500}
                     height={100}
                     alt='Dentist with patient'
                     priority
-                />
+                /> */}
             </div>
         </div>
     )
