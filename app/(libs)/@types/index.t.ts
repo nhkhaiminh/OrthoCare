c76875f76ed82a39d621663a3eed82ea650dcd6e
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-12-17 13:21:39 -0500

    Add DentalServices type

--- app/(libs)/@types/index.t.ts
+++ app/(libs)/@types/index.t.ts
@@ -6,3 +6,10 @@ export type MobileNavbarContextType = {
     openState: IMobileNavbar['openState']
     setOpenState: (openState: IMobileNavbar['openState']) => void;
 }
+
+export type DentalServices = {
+    image: string;
+    title: string;
+    link?: string;
+    last?: boolean;
+}
